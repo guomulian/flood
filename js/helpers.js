538399c758ebbb8593c39cf02855b28020a85216
Anne Kwok <guomulian@gmail.com>
2018-01-25 11:51:37 -0800

    Changed maxAllowedMoves function.
    Changed maxAllowedMoves (again).

--- js/helpers.js
+++ js/helpers.js
@@ -4,5 +4,5 @@ function randomIndex(n) {
 
 /* Come up with a better function for this. */
 function maxAllowedMoves(number_of_colors, size) {
-    return Math.floor(number_of_colors * size * 0.5);
+    return Math.floor(number_of_colors * size * 0.4);
 }
